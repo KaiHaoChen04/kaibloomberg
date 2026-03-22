5704476c054a637aac2046e34efb992328bf39ae
kai <chenkenny414@gmail.com>
2026-03-22 15:29:57 -0400

    async run terminal via app

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+mod app;
+mod app_data;
+mod ui;
+
+use app::App;
+use ui::run_ui;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut app = App::new();
+    run_ui(&mut app).await
+}
