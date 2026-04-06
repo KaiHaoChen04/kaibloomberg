b18349acc0769f91836d99d1ad3cea98ebb7881b
kai <chenkenny414@gmail.com>
2026-04-06 18:28:44 -0400

    add to holdings list listening to keyevent

--- src/app_data/holdings.rs
+++ src/app_data/holdings.rs
@@ -1,16 +1,24 @@
 use std::collections::HashMap;
+use crossterm::event::{KeyCode, KeyEvent};
 use serde::Deserialize;
 
-#[derive(Debug, Clone, Deserialize, Default)]
+#[derive(Debug, Clone, Deserialize)]
 pub struct Stock {
     average_price: f64,
     quantity: f64,
-    symbol: String,
 }
 #[derive(Debug, Clone, Deserialize, Default)]
 #[serde(transparent)]
 pub struct Holdings {
     pub holding_list: HashMap<String, Stock>,
+}
+impl Default for Stock {
+    fn default() -> Self {
+        Self { 
+            average_price: 0.0, 
+            quantity: 0.0 
+        }
+    }
 }
 
 impl Stock {
@@ -25,5 +33,23 @@ impl Stock {
             0.0
         };
         (profit_loss, profit_loss_percentage)
+    }
+}
+pub fn add_to_list(hold: &mut Holdings,key: KeyEvent, input_buffer: &mut String) {
+    match key.code {
+        KeyCode::Char(ch) => {
+            input_buffer.push(ch);
+        }
+        KeyCode::Backspace => {
+            input_buffer.pop();
+        }
+        KeyCode::Enter => {
+            let symbol = input_buffer.trim().to_uppercase();
+            if !symbol.is_empty() {
+                hold.holding_list.entry(symbol).or_insert(Stock::default());
+            }
+            input_buffer.clear();
+        }
+        _ => {}
     }
 }
