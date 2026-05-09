b6ab1e3a41b25a23784c3ff4e0ecebaf8701236a
kai <chenkenny414@gmail.com>
2026-05-09 11:47:12 -0400

    drew options frame

--- src/ui/options.rs
+++ src/ui/options.rs
@@ -3,18 +3,31 @@ use ratatui::{
     layout::{Constraint, Direction, Layout, Rect},
     style::Modifier,
     style::{Color, Style},
-    widgets::{Block, Borders, Paragraph, Row, Table},
+    widgets::{Block, Borders, Row, Table},
 };
 
-pub fn draw_options_chart(frame: &Frame, area: Rect) {
-    let call_puts_box = Layout::default()
-        .direction(Direction::Horizontal)
-        .constraints([
-            Constraint::Percentage(50),
-            Constraint::Percentage(50),
-        ])
-        .split(area);
+pub fn draw_options_chart(frame: &mut Frame, area: Rect) {
 
-    
+    let header_col = Row::new(
+        vec!["Last Trade Date", "Strike", "Last Price", "Bid", "Ask", "Volume", "Open Interest", "Implied Volatility"])
+        .style(
+            Style::default()
+                .fg(Color::LightYellow)
+                .add_modifier(Modifier::BOLD)
+        )
+        .bottom_margin(1);
 
+    let rows = vec![
+        Row::new(vec!["-", "-", "-", "-", "-", "-", "-", "-"])];
+
+    let widths = [
+        Constraint::Percentage(12); 8
+    ];
+
+    let table = Table::new(rows, widths)
+        .header(header_col)
+        .block(Block::default().title(" Options ").borders(Borders::ALL))
+        .column_spacing(2);
+
+    frame.render_widget(table, area);
 }
