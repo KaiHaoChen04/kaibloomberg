6fd998cc7dd699ec2b506e3bdcade30391c99d9d
kai <chenkenny414@gmail.com>
2026-07-07 11:01:37 -0400

    title fetch fn

--- src/app_data/news.rs
+++ src/app_data/news.rs
@@ -22,7 +22,15 @@ impl News {
 }
 
 async fn fetch_and_parse_title(source: News) -> Result<Vec<String>, Box<dyn Error>> {
+    let response = reqwest::get(source.url()).await?.bytes().await?;
+    let channel = Channel::read_from(&response[..])?;
 
+    let titles = channel
+        .items()
+        .iter()
+        .filter_map(|item| item.title().map(|t| t.to_string()))
+        .collect();
 
+    Ok(titles)
 
 }
