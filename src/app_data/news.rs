e3db8cff24de43c390d3d84e485dcc7d63c3166b
kai <chenkenny414@gmail.com>
2026-07-06 20:50:30 -0400

    basic structure of news

--- src/app_data/news.rs
+++ src/app_data/news.rs
@@ -1,5 +1,20 @@
 
+pub enum News {
+    Tech,
+    Finance,
+}
 
-pub struct News {
-    //need to find corresponding api response 
+impl News {
+    fn url(&self) -> &'static str {
+        match self {
+            News::Finance => "https://finance.yahoo.com/news/rssindex",
+            News::Tech => "https://news.yahoo.com/rss/tech",
+        }
+    }
+    fn label(&self) -> &'static str {
+        match self {
+            News::Finance => "World Finance",
+            News::Tech => "Technology",
+        }
+    }
 }
